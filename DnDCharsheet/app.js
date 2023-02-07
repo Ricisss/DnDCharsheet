2283b6c6a75befe876645258753ddf18f2f9abd1
Richard Smith <qpalymmm@gmail.com>
2023-02-07 21:46:34 +0100

    implemented panel 1 and a half

--- DnDCharsheet/app.js
+++ DnDCharsheet/app.js
@@ -6,7 +6,7 @@ const port = 8080;
 
 app.listen(port);
 app.use(express.static('js_output'));
-app.use(express.static('css'));
+app.use(express.static('styles'));
 
 app.get('/', function (request, response) {
     response.sendFile(__dirname + '/html/index.html');
